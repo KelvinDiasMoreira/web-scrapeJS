3b8e180faf8b3cf07f63944c8f88112d31af25ee
Kelvi <kelvingremista2015@hotmail.com>
2024-02-19 23:00:57 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
+import fs from 'fs'
+import { Worker } from 'worker_threads'
 
-const URL = "https://investidor10.com.br/acoes/bbse3/";
+const getStocks = async() => {
+  try{
+    const { data } = await axios.get('https://brapi.dev/api/available?search=&token=eJGEyu8vVHctULdVdHYzQd')
+    const { stocks } = data
+    return stocks
+  }catch(err){
+    console.log(err)
+  }
+}
+
+// const URL = "https://investidor10.com.br/acoes/bbse3/";
 
 const searchInArray = [
   "Cotação",
@@ -31,28 +43,47 @@ const setItensArray = searchInArray.map((_, i) => ({
   [i === 0 ? i : i % 2 === 0 && i]: { name: `${newNames[i]}` },
 }));
 
-const getData = async () => {
+const dataToJson = []
+
+const getData = async (stockToSearch, index) => {
   try {
-    const { data } = await axios.get(URL);
+    console.log(`Catching data of ${stockToSearch} ....`)
+    const { data } = await axios.get(`https://investidor10.com.br/acoes/${stockToSearch}/`);
     const $ = cheerio.load(data);
     const sectionCards = $("#cards-ticker span");
     const dataSectionCards = [];
-
     for (let i = 0; i < sectionCards.length; i++) {
       if (sectionCards[i].children) {
         dataSectionCards.push(sectionCards[i].children[0].data);
       }
     }
-    
     const dataFormatted = {};
     for (let i = 0; i < dataSectionCards.length; i++) {
       if (i % 2 === 0) {
-        dataFormatted[setItensArray[i][i].name] = dataSectionCards[i + 1];
+        dataFormatted[setItensArray[i][i].name] = dataSectionCards[i + 1]
+        console.log("🚀 ~ getData ~ dataFormatted:", dataFormatted)
       }
     }
-  } catch (er) {
-    console.log(err);
+    dataToJson.push({[stockToSearch]: dataFormatted})
+    console.log("🚀 ~ dataToJson:", dataToJson)
+  } catch (err) {
+    if(err){
+      console.log(`not found stock ${stockToSearch}`)
+      return
+    }
+    // console.log(err);
   }
 };
 
-getData();
+//TODO, use multi-threaded to do this searchs
+
+const stocks = await getStocks()
+if(!stocks.length){
+  throw new Error("Not found stocks to search")
+}
+
+for(let i = 0;i < stocks.length; i++){
+  await getData(stocks[i], i);
+}
+
+
